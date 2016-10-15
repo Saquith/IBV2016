058c39def67620347110b1f91d383a01917a04f8
TimBakx <timbakx.student@gmail.com>
2016-10-15 20:17:33 +0200

    Added grayscale, threshold and a screenshot
    Added new processes for computing grayscale image, threshold image and
    the screenshot for the assignment plan.

--- INFOIBV/Form1.cs
+++ INFOIBV/Form1.cs
@@ -52,6 +52,26 @@ namespace INFOIBV
             }
         }
 
+        public class Grayscale : Processor
+        {
+            public override Color Transform(Color c)
+            {
+                int grayVal = (int)((c.R * 0.21) + (c.G * 0.72) + (c.B * 0.07)); // The numbers represent a weight for better grayscale values
+                return Color.FromArgb(grayVal, grayVal, grayVal);
+            }
+        }
+
+        public class Threshold : Processor
+        {
+            public override Color Transform(Color c)
+            {
+                int t = 128;    // Threshold value
+
+                if (c.R > t) { return Color.White; }
+                else return Color.Black;
+            }
+        }
+
         private void applyButton_Click(object sender, EventArgs e)
         {
             if (InputImage == null) return;                                 // Get out if no input image
@@ -62,8 +82,10 @@ namespace INFOIBV
             //TODO use your own processor(s) here
             Processor processor = new MultiProcessor(new Processor[]
             {
-                new Negative(),
-                new RotateRight()
+                new Grayscale(), 
+                new Threshold()
+                //new Negative(),
+                //new RotateRight()
             });
             OutputImage = processor.Process(InputImage);                   // Process the image
 
